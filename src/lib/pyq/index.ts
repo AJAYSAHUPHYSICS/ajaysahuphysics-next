ad652868a3d8f8e3c8ffacf5b90add25250817b5
AJAYSAHUPHYSICS <sahua8675@gmail.com>
2026-07-11 22:27:50 +0530

    chore(pyq): register Ray Optics & Optical Instruments in pyq registry

--- src/lib/pyq/index.ts
+++ src/lib/pyq/index.ts
@@ -17,6 +17,7 @@ import { magnetismAndMatterPyq } from "./magnetism-and-matter";
 import { electromagneticInductionPyq } from "./electromagnetic-induction";
 import { alternatingCurrentPyq } from "./alternating-current";
 import { electromagneticWavesPyq } from "./electromagnetic-waves";
+import { rayOpticsAndOpticalInstrumentsPyq } from "./ray-optics-and-optical-instruments";
 
 export const pyqRegistry: Record<string, ChapterPyq> = {
   "basic-mathematics-and-vectors": basicMathematicsAndVectorsPyq,
@@ -37,6 +38,7 @@ export const pyqRegistry: Record<string, ChapterPyq> = {
   "electromagnetic-induction": electromagneticInductionPyq,
   "alternating-current": alternatingCurrentPyq,
   "electromagnetic-waves": electromagneticWavesPyq,
+  "ray-optics-and-optical-instruments": rayOpticsAndOpticalInstrumentsPyq,
 };
 
 export function getChapterPyq(slug: string): ChapterPyq | undefined {
